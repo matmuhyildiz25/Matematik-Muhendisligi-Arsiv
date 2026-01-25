1c45c85ca425cea955ef811ee928eb8393bf3259
matmuhyildiz25 <matmuhyildiz@gmail.com>
2026-01-25 22:14:05 +0000

    dosyalar

---
+++ Prog/genelBilgiler.c
@@ -0,0 +1,55 @@
+/////////////////////////////////////////////////
+    /*
+    %c : char
+    %d : int
+    %f : float
+    %ld : long int
+    %s : textual expression
+    %u : unsigned int
+    %lf : double
+    */
+/////////////////////////////////////////////////
+    /*
+    \b : deleting the character before it
+    \t : put tab space
+    \" : prints " "
+    */
+/////////////////////////////////////////////////
+    /*
+    + : sum
+    - : substraction
+    * : multiplication
+    / : division
+    % : mod
+    ++ 
+    --
+    */
+/////////////////////////////////////////////////
+    /*
+    += : x+=2     x = x + 2
+    -= : x-=2     x = x - 2
+    *= : x*=2     x = x * 2
+    /= : x/=2     x = x / 2
+    %= : x%=2     x = x % 2
+    &= : x&=2     x = x & 2
+    |= : x|=2     x = x | 2
+    ^= : x^=2     x = x ^ 2
+    >>= : x>>=2   x = x >> 2
+    <<= : x<<=2   x = x << 2
+    */
+/////////////////////////////////////////////////
+    /*
+    Comparasion operators
+    == : equal to
+    != : not equal
+    > : greater than
+    < : less than
+    >= : greater than or equal to
+    <= : less than or equal to
+
+    Logical operations
+    && : logical and (both conditions need to be satified)
+    || : or (veya)
+    ^  : xor (ya da)
+    */
+/////////////////////////////////////////////////
